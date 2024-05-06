1089dc64f45e5025ab80a675f288ca3bf57c50d5
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-06 12:06:33 +0200

    Modbus PDU: REFACTOR

--- src/modbus_PDU.c
+++ src/modbus_PDU.c
@@ -11,27 +11,29 @@
 #include "modbus_PDU.h"
 
 static void write_u16_to_buf(uint8_t *buf,uint16_t data);
-
-void modbus_master_read_holding_reg(uint8_t *send_buf, modbus_adr_t adr, modbus_reg_qty_t len)
-{
-    send_buf[0] = READ_HOLDING_REGISTERS;
-
-    write_u16_to_buf(send_buf+1,adr);
-    write_u16_to_buf(send_buf+3,len);
-}
-
-void modbus_master_read_input_reg(uint8_t *send_buf, modbus_adr_t adr, modbus_reg_qty_t len)
-{
-    send_buf[0] = READ_INPUT_REGISTERS;
-
-    write_u16_to_buf(send_buf+1,adr);
-    write_u16_to_buf(send_buf+3,len);
-}
-
-
+static void read_reg_request(uint8_t *send_buf,uint8_t req_code, modbus_adr_t adr, modbus_reg_qty_t len);
 
 static void write_u16_to_buf(uint8_t *buf,uint16_t data)
 {
     buf[0] = (uint8_t)(data >> 8);
     buf[1] = (uint8_t)(data & 0xFF);
 }
+
+static void read_reg_request(uint8_t *send_buf,uint8_t req_code, modbus_adr_t adr, modbus_reg_qty_t len)
+{
+    send_buf[0] = req_code;
+
+    write_u16_to_buf(send_buf+1,adr);
+    write_u16_to_buf(send_buf+3,len);
+}
+
+void modbus_master_read_holding_reg(uint8_t *send_buf, modbus_adr_t adr, modbus_reg_qty_t len)
+{
+    read_reg_request(send_buf,READ_HOLDING_REGISTERS,adr,len);
+}
+
+void modbus_master_read_input_reg(uint8_t *send_buf, modbus_adr_t adr, modbus_reg_qty_t len)
+{
+    read_reg_request(send_buf,READ_INPUT_REGISTERS,adr,len);
+}
+
