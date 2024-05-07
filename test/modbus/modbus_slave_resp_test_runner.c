5426c4ca2024f307171af2bbf34075c954fd764a
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-07 18:28:58 +0200

    Test: GREEN  added new test ->
    SlaveRead8DiscreteInputs

--- test/modbus/modbus_slave_resp_test_runner.c
+++ test/modbus/modbus_slave_resp_test_runner.c
@@ -10,6 +10,10 @@ TEST_GROUP_RUNNER(Modbus_Slave_Resp)
    RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead16Coils);
    RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead17Coils);
    RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead5DiscreteInputs);
+   RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead8DiscreteInputs);
+   // RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead9DiscreteInputs);
+   // RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead16DiscreteInputs);
+   // RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead17DiscreteInputs);
    // RUN_TEST_CASE(Modbus_Slave_Resp, );
    // RUN_TEST_CASE(Modbus_Slave_Resp, );
    // RUN_TEST_CASE(Modbus_Slave_Resp, );
