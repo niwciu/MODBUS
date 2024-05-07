84662b5a699af310bfb062e3ee9bcf518e105cfb
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-07 14:38:03 +0200

    Test: GREEN added new test ->
    SlaveRead16Coils

--- test/modbus/modbus_slave_resp_test_runner.c
+++ test/modbus/modbus_slave_resp_test_runner.c
@@ -7,7 +7,7 @@ TEST_GROUP_RUNNER(Modbus_Slave_Resp)
    RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead5Coils);
    RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead8Coils);
    RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead9Coils);
-   // RUN_TEST_CASE(Modbus_Slave_Resp, );
+   RUN_TEST_CASE(Modbus_Slave_Resp, SlaveRead16Coils);
    // RUN_TEST_CASE(Modbus_Slave_Resp, );
    // RUN_TEST_CASE(Modbus_Slave_Resp, );
    // RUN_TEST_CASE(Modbus_Slave_Resp, );
