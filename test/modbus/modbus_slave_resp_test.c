b32b007dccf11c6245ac65f38003f53ec431ded9
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-07 18:32:25 +0200

    Test: GREEN added new test ->
    SlaveRead17DiscreteInputs

--- test/modbus/modbus_slave_resp_test.c
+++ test/modbus/modbus_slave_resp_test.c
@@ -170,5 +170,23 @@ TEST(Modbus_Slave_Resp, SlaveRead16DiscreteInputs)
     TEST_ASSERT_EQUAL_HEX8(exp_readed_din_value[0], resp_PDU[2]);
     TEST_ASSERT_EQUAL_HEX8(exp_readed_din_value[1], resp_PDU[3]);
 }
+
+TEST(Modbus_Slave_Resp, SlaveRead17DiscreteInputs)
+{
+    modbus_adr_t adr = 0x0003;
+    modbus_data_qty_t din_qty = 17;
+    modbus_byte_count_t expected_byte_count = 3; // in each byte 8 coil status is reported
+    uint8_t exp_readed_din_value[3] = {0x55,0x55,0x01}; //0b0001 0101
+    mock_set_expected_disc_in_alternately(adr,din_qty);
+
+    modbus_master_read_discrete_inputs(req_PDU, adr, din_qty);
+    modbus_slave_read_discrete_inputs(resp_PDU, req_PDU);
+
+    TEST_ASSERT_EQUAL_UINT8(MODBUS_READ_DISCRETE_INPUTS_FUNC_CODE, resp_PDU[0]);
+    TEST_ASSERT_EQUAL(expected_byte_count, resp_PDU[1]);
+    TEST_ASSERT_EQUAL_HEX8(exp_readed_din_value[0], resp_PDU[2]);
+    TEST_ASSERT_EQUAL_HEX8(exp_readed_din_value[1], resp_PDU[3]);
+    TEST_ASSERT_EQUAL_HEX8(exp_readed_din_value[2], resp_PDU[4]);
+}
 //
 
