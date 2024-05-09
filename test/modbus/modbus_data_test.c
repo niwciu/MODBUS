fee634e964894785438afb590a719562a3fc0169
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-09 12:25:59 +0200

    Test: GREEN added new test ->
    ReadRegisteredDiscreteInput

--- test/modbus/modbus_data_test.c
+++ test/modbus/modbus_data_test.c
@@ -50,14 +50,14 @@ TEST(Modbus_Data, RegisterDiscreteInputData)
     TEST_ASSERT_EQUAL(&app_din_data,Discrete_Inputs[din_adr]);
 }
 
-// TEST(Modbus_Data, ReadRegisteredDiscreteInput)
-// {
-//     modbus_coil_t app_din_data=1;
-//     modbus_adr_t din_adr=0x0001;
-//     register_app_data_to__table(din_adr,&app_din_data);
+TEST(Modbus_Data, ReadRegisteredDiscreteInput)
+{
+    modbus_coil_t app_din_data=1;
+    modbus_adr_t din_adr=0x0001;
+    register_app_data_to_discrete_input_table(din_adr,&app_din_data);
     
-//     TEST_ASSERT_EQUAL(app_din_data,get_discrete_input_state(coil_adr));
-// }
+    TEST_ASSERT_EQUAL(app_din_data,get_discrete_input_state(din_adr));
+}
 
 //
 // testy na zerową ilość rejestrów coili do odczytu zapisu. 
--- test/modbus/modbus_data_test_runner.c
+++ test/modbus/modbus_data_test_runner.c
@@ -9,7 +9,7 @@ TEST_GROUP_RUNNER(Modbus_Data)
    RUN_TEST_CASE(Modbus_Data, ReadUnregisteredCoil);
 
    RUN_TEST_CASE(Modbus_Data, RegisterDiscreteInputData);
-   // RUN_TEST_CASE(Modbus_Data, ReadRegisteredDiscreteInput);
+   RUN_TEST_CASE(Modbus_Data, ReadRegisteredDiscreteInput);
    // RUN_TEST_CASE(Modbus_Data, );
    // RUN_TEST_CASE(Modbus_Data, );
    // RUN_TEST_CASE(Modbus_Data, );
