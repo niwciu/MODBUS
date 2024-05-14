2145ff08ad84d1dc53a3a5ebfa36b97378f3e013
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-14 10:39:55 +0200

    Project: added modbus_RTU module .c and .h files

---
+++ src/modbus_RTU.c
@@ -0,0 +1,9 @@
+/**
+ * @file modbus_RTU.c
+ * @author niwciu (niwciu@gmail.com)
+ * @brief 
+ * @date 2024-05-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
---
+++ src/modbus_RTU.h
@@ -0,0 +1,23 @@
+/**
+ * @file modbus_RTU.h
+ * @author niwciu (niwciu@gmail.com)
+ * @brief 
+ * @date 2024-05-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef _MODBUS_RTU_H_
+#define _MODBUS_RTU_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+#include "modbus_type.h"
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* _MODBUS_RTU_H_ */
