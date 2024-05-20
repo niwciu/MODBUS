45380a6dc751a19cb1314830ad5cca4f8c0de4b0
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-20 12:51:54 +0200

    modbus_PDU: GREEN test ->
    GivenSlaveRespondWithCorrectFunctionCodeWhenMasterWriteSingleRegisterRespAndAddresIsIncorrectThenMasterWriteSingleRegisterRespRetErrorOutAddr

--- src/modbus_PDU.c
+++ src/modbus_PDU.c
@@ -304,7 +304,13 @@ modbus_ret_t modbus_master_write_single_coil_resp(const modbus_buf_t *resp_buf, 
 
 modbus_ret_t modbus_master_write_single_reg_resp(const  modbus_buf_t *resp_buf, const modbus_buf_t *req_buf)
 {
-    return RET_OK;
+    modbus_ret_t status = RET_OK;  
+    if (resp_buf[MODBUS_RESP_WRITE_ADR_IDX] == req_buf[MODBUS_REQUEST_ADR_IDX])
+        status = RET_OK;
+    else
+        status = RET_ERROR_WRITE_SINGLE_OUT_ADR;
+    
+    return status;
 }
 
 // Slave API functions
