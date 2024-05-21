a2018e9edfe4d4c664044028708ea9c6ec989975
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-21 10:18:40 +0200

    Test: GREEN added new test ->
    GivenSlaveRespondWithCorectFunctionCodeAndCorrectStartAdrAndIncorrectOutputQtyWhenMasterWriteMultiRegRespThenRetOutQtyError

--- test/modbus/modbus_master_read_test.c
+++ test/modbus/modbus_master_read_test.c
@@ -478,19 +478,19 @@ TEST(Modbus_Master_Read, GivenSlaveRespondWithCorectFunctionCodeAndIncorrectStar
     TEST_ASSERT_EQUAL_INT16(RET_ERROR_WRITE_OUT_ADR,modbus_master_write_multiple_reg_resp(resp_msg,req_msg));
 }
 
-// TEST(Modbus_Master_Read, GivenSlaveRespondWithCorectFunctionCodeAndCorrectStartAdrAndIncorrectOutputQtyWhenMasterWriteMultiRegRespThenRetOutQtyError)
-// {
-//     modbus_adr_t reg_adr =0x0005;
-//     modbus_data_qty_t reg_qty=30;
+TEST(Modbus_Master_Read, GivenSlaveRespondWithCorectFunctionCodeAndCorrectStartAdrAndIncorrectOutputQtyWhenMasterWriteMultiRegRespThenRetOutQtyError)
+{
+    modbus_adr_t reg_adr =0x0005;
+    modbus_data_qty_t reg_qty=30;
 
-//     mock_set_expected_master_hreg_alternately(reg_adr,reg_qty,0x5A5A);
-//     modbus_master_write_multiple_reg_req(req_msg,reg_adr,reg_qty);
-//     modbus_slave_write_multiple_reg(resp_msg,req_msg);
+    mock_set_expected_master_hreg_alternately(reg_adr,reg_qty,0x5A5A);
+    modbus_master_write_multiple_reg_req(req_msg,reg_adr,reg_qty);
+    modbus_slave_write_multiple_reg(resp_msg,req_msg);
 
-//     write_u16_to_buf(&resp_msg[MODBUS_RESP_WRITE_MULTIPLE_DATA_QTY_IDX],reg_qty+1);
+    write_u16_to_buf(&resp_msg[MODBUS_RESP_WRITE_MULTIPLE_DATA_QTY_IDX],reg_qty+1);
 
-//     TEST_ASSERT_EQUAL_INT16(RET_ERROR_WRITE_MULTI_OUT_QTY,modbus_master_write_multiple_reg_resp(resp_msg,req_msg));
-// }
+    TEST_ASSERT_EQUAL_INT16(RET_ERROR_WRITE_MULTI_OUT_QTY,modbus_master_write_multiple_reg_resp(resp_msg,req_msg));
+}
 // TEST(Modbus_Master_Read, )
 // {
 //     TEST_FAIL_MESSAGE ("ADDED NEW TEST");
