bcca52136c130aaa0703ae0fb594f86606b3abec
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-24 22:12:59 +0200

    HW_test: Replace timer7 with USART timer ovf for idel state

---
+++ test/hw_test/STM32G070RB_MODBUS_MASTER/Drivers/STM32G0_Drivers/modbus_driver_config.h
@@ -0,0 +1,10 @@
+/**
+ * @file modbus_driver_config.h
+ * @author niwciu (niwciu@gmail.com)
+ * @brief 
+ * @version 0.0.1
+ * @date 2024-05-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
--- test/hw_test/STM32G070RB_MODBUS_MASTER/Drivers/STM32G0_Drivers/modbus_driver_interface.c
+++ test/hw_test/STM32G070RB_MODBUS_MASTER/Drivers/STM32G0_Drivers/modbus_driver_interface.c
@@ -89,6 +89,7 @@ const struct modbus_RTU_driver_struct *get_RTU_driver_interface(void);
 
 static void USART_GPIO_init(void);
 static void USART_module_config(uint32_t Baud, parity_t parity);
+static uint32_t get_RTOR_value(uint32_t baud);
 static void USART_data_init(void);
 
 static const struct modbus_RTU_driver_struct RTU_driver = {
@@ -115,17 +116,6 @@ static void usart_init(uint32_t Baud, parity_t parity) // parameters for init ->
     NVIC_EnableIRQ(MODBUS_USART_IRQN);
     // ToDo init rx tx buffers as a function
     rx_buf.data_idx = 0;
-
-    /* Timer configuration for measuring 1,5 and 3,5 char*/
-    // ToDo get ARR value fo 3,5 char depanding on Baud Rate
-    RCC->APBENR1 |= MODBUS_TIMER_CLK_EN;
-    MODBUS_TIMER->CR1 |= TIM_CR1_OPM | TIM_CR1_URS;
-    MODBUS_TIMER->DIER = TIM_DIER_UIE;
-    MODBUS_TIMER->PSC = MODBUS_TIMER_CLK_FREQ / 1000000 - 1;
-    MODBUS_TIMER->ARR = 1750;
-
-    NVIC_SetPriority(MODBUS_TIMER_IRQN, MODBUS_TIMER_IRQ_PRIORITY);
-    NVIC_EnableIRQ(MODBUS_TIMER_IRQN);
 }
 
 static void usart_send(modbus_buf_t *tx_msg, uint8_t msg_len)
@@ -155,16 +145,11 @@ void MODBUS_USART_IRQHandler(void)
     // ToDo check in documentation which interrupt need to be also take cared -> errors overrun etc
     if ((MODBUS_USART->ISR) & USART_ISR_RXNE_RXFNE)
     {
-        // ToDo check if you are not between 1,5 char and 3,5 char timing (frame error)
-        // if frame error accure set error flag and skip collecting data in rx buf
         rx_buf.data[rx_buf.data_idx] = MODBUS_USART->RDR;
         rx_buf.data_idx++;
-        MODBUS_TIMER->EGR |= TIM_EGR_UG;
-        MODBUS_TIMER->CR1 |= TIM_CR1_CEN;
     }
-    else if ((MODBUS_USART->ISR) & USART_ISR_TXE_TXFNF)
+    if ((MODBUS_USART->ISR) & USART_ISR_TXE_TXFNF)
     {
-
         if (tx_buf.cur_byte_ptr < tx_buf.last_byte_ptr)
         {
             tx_buf.cur_byte_ptr++;
@@ -175,16 +160,12 @@ void MODBUS_USART_IRQHandler(void)
             MODBUS_USART->CR1 &= ~(USART_CR1_TXEIE_TXFNFIE);
         }
     }
-}
-
-void MODBUS_TIMER_IRQHandler(void)
-{
-    if (MODBUS_TIMER->SR & TIM_SR_UIF)
+    if ((MODBUS_USART->ISR) & USART_ISR_RTOF)
     {
-        MODBUS_TIMER->SR &= ~TIM_SR_UIF;
         rx_buf.data_idx = 0;
         // if error error flag set do nothign
         //  else pass the rx buf to rsp msg or set some flag to indicate that there are some data to parse
+        MODBUS_USART ->ICR |= USART_ICR_RTOCF;
     }
 }
 
@@ -201,6 +182,7 @@ static void USART_GPIO_init(void)
 }
 static void USART_module_config(uint32_t Baud, parity_t parity)
 {
+
     // enable clk
     RCC->MODBUS_USART_CLK |= MODBUS_USART_CLK_EN;
     // set all bits in CR1 register
@@ -227,10 +209,26 @@ static void USART_module_config(uint32_t Baud, parity_t parity)
     MODBUS_USART->CR3 |= (USART_CR3_OVRDIS | USART_CR3_DEM);
     // set expected baudrate
     MODBUS_USART->BRR = MODBUS_USART_CLK_FREQ / Baud; // for over8=1 CLk_FRE must be multiply by 2
-
+    // set modbus delay for recognizing end of frame
+    
+    MODBUS_USART -> RTOR |= get_RTOR_value(Baud);
     // enable usart module
     MODBUS_USART->CR1 |= (USART_CR1_UE);
 }
+static uint32_t get_RTOR_value(uint32_t baud)
+{
+    if(baud<=19200)
+        return 38;
+    else
+    {
+        uint32_t q=baud/9600;
+        q=q*168;
+        q=q/10;
+        if(q>0xFFFFFF) q=0xFFFFFF;
+        return q;
+    }
+}
 static void USART_data_init(void)
 {
+   
 }
