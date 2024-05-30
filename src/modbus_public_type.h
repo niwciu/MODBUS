76a194e706020372821b08a55196b5426c1b8708
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-30 11:19:49 +0200

    REFACTOR: -> move type.h to common internall folder, create public type .h

---
+++ src/modbus_public_type.h
@@ -0,0 +1,23 @@
+/**
+ * @file modbus_public_type.h
+ * @author niwciu (niwciu@gmail.com)
+ * @brief 
+ * @date 2024-05-30
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef _MODBUS_PUBLIC_TYPE_H_
+#define _MODBUS_PUBLIC_TYPE_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* _MODBUS_PUBLIC_TYPE_H_ */
