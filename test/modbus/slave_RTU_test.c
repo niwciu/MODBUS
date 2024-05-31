caedf91d49c38384b0f4a551eb427102d1e985f2
niwciu <57457942+niwciu@users.noreply.github.com>
2024-05-31 23:44:34 +0200

    RED: added new test
    GivenModbusSlaveInitAndReadCoilsReqWithIncorectSlaveIdRecivedWhenTimer1_5CharTrigerThenSkiMsgGoBackToIdleState

--- test/modbus/slave_RTU_test.c
+++ test/modbus/slave_RTU_test.c
@@ -1,12 +1,26 @@
 #include "unity/fixture/unity_fixture.h"
+#include "modbus_slave.h"
+#include "modbus_master_PDU.h"
+#include "mock_slave_driver_interface.h"
+// #include <mem.h>
+// #include "modbus_public_type.h"
 
 // #include "tested_module.h"
 
 TEST_GROUP(Slave_RTU_test);
 
+extern modbus_slave_state_t slave_manager_state_machine;
+extern modbus_msg_t *slave_msg_buf;
+
+modbus_device_ID_t device_ID = 0x12;
+baud_t baud = 38400;
+parity_t parity = ODD;
+modbus_mode_t mode = RTU;
+
 TEST_SETUP(Slave_RTU_test)
 {
     /* Init before every test */
+    modbus_slave_init(mode,baud,parity,device_ID);
 }
 
 TEST_TEAR_DOWN(Slave_RTU_test)
@@ -14,7 +28,14 @@ TEST_TEAR_DOWN(Slave_RTU_test)
     /* Cleanup after every test */
 }
 
-TEST(Slave_RTU_test, FirstTest)
+TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithIncorectSlaveIdRecivedWhenTimer1_5CharTrigerThenSkiMsgGoBackToIdleState)
 {
-    TEST_FAIL_MESSAGE("Implement your test!");
+    modbus_adr_t coil_adr=0x0001;
+    modbus_data_qty_t coils_qty=2;
+    
+    modbus_master_read_coils_req(slave_msg_buf,coil_adr,coils_qty);
+
+    mock_msg_rx_done_cb(); 
+    TEST_ASSERT_EQUAL(MODBUS_SLAVE_IDLE, slave_manager_state_machine);
+    TEST_ASSERT_EQUAL(0,slave_msg_buf->req.len);
 }
--- test/modbus/slave_RTU_test_runner.c
+++ test/modbus/slave_RTU_test_runner.c
@@ -3,5 +3,18 @@
 TEST_GROUP_RUNNER(Slave_RTU_test)
 {
    /* Test cases to run */
-   RUN_TEST_CASE(Slave_RTU_test, FirstTest);
+   RUN_TEST_CASE(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithIncorectSlaveIdRecivedWhenTimer1_5CharTrigerThenSkiMsgGoBackToIdleState);
+//    RUN_TEST_CASE(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdRecivedWhenTimer1_5CharTrigerThen);
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
+//    RUN_TEST_CASE(Slave_RTU_test, );
 }
