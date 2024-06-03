5d8e47d33278abd946c77a821aeda090d7df7d6d
niwciu <57457942+niwciu@users.noreply.github.com>
2024-06-03 14:44:38 +0200

    RED: added new test ->
    GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndIncorrectCrcRecivedAndReciverTimer1_5CharTrigerWhenT3_5timerTriegerdThenIgnoreFrameAndGoToIdleState

--- test/modbus/slave_RTU_test.c
+++ test/modbus/slave_RTU_test.c
@@ -3,6 +3,7 @@
 #include "modbus_master_PDU.h"
 #include "modbus_RTU.h"
 #include "mock_slave_driver_interface.h"
+#include "buf_rw.h"
 // #include <mem.h>
 // #include "modbus_public_type.h"
 
@@ -17,6 +18,8 @@ modbus_device_ID_t device_ID = 0x12;
 baud_t baud = 38400;
 parity_t parity = ODD;
 modbus_mode_t mode = RTU;
+
+static void modify_CRC_in_msg_frame(void);
 
 TEST_SETUP(Slave_RTU_test)
 {
@@ -65,7 +68,7 @@ TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithIncorectSlaveIdReciv
     TEST_ASSERT_EQUAL(0,slave_msg_buf->req.len);
 }
 
-TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndProperCRCRecivedWhenTimer1_5CharTrigerThenGoToModbusSlaveReciverSilancePendingState)
+TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndProperCrcRecivedWhenTimer1_5CharTrigerThenGoToModbusSlaveReciverSilancePendingState)
 {
     modbus_adr_t coil_adr=0x0001;
     modbus_data_qty_t coils_qty=2;
@@ -83,13 +86,39 @@ TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndProp
     TEST_ASSERT_EQUAL(MODBUS_SLAVE_RECIVER_SILANCE_PENDING, slave_manager_state_machine);
 }
 
-TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndReciverTimer1_5CharTrigerWhenRxCharBeforeT3_5timerTriegerdThenIgnoreFrameAndGoToIdleState)
+
+TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndProperCrcAndReciverTimer1_5CharTrigerWhenRxCharBeforeT3_5timerTriegerdThenIgnoreFrameAndGoToIdleState)
 {
     modbus_adr_t coil_adr=0x0001;
     modbus_data_qty_t coils_qty=2;
 
     modbus_master_read_coils_req(slave_msg_buf,coil_adr,coils_qty);
     modbus_RTU_send(slave_msg_buf->req.data,&slave_msg_buf->req.len, device_ID);
+    mock_USART_RX_IRQ(); // To simulate that some char was recived after using modbus_RTU_send func to generate recived request
+
+    TEST_ASSERT_EQUAL(MODBUS_SLAVE_IDLE, slave_manager_state_machine);
+    mock_1_5_char_timer_IRQ();
+    TEST_ASSERT_EQUAL(MODBUS_SLAVE_IDLE, slave_manager_state_machine); 
+    check_modbus_request();
+    TEST_ASSERT_EQUAL(MODBUS_SLAVE_MSG_RECIVED, slave_manager_state_machine);
+    check_modbus_request();
+    TEST_ASSERT_EQUAL(MODBUS_SLAVE_RECIVER_SILANCE_PENDING, slave_manager_state_machine);
+    mock_USART_RX_IRQ(); //this is recived data before 3,5char time expired. According to modbus doc such event should trigger frame error flag and msg should be ignored after 3,5 char expired
+    check_modbus_request();
+    mock_3_5_char_timer_IRQ();
+    check_modbus_request();
+    TEST_ASSERT_EQUAL(MODBUS_SLAVE_IDLE, slave_manager_state_machine);
+}
+
+TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndIncorrectCrcRecivedAndReciverTimer1_5CharTrigerWhenT3_5timerTriegerdThenIgnoreFrameAndGoToIdleState)
+{
+    modbus_adr_t coil_adr=0x0001;
+    modbus_data_qty_t coils_qty=2;
+   
+
+    modbus_master_read_coils_req(slave_msg_buf,coil_adr,coils_qty);
+    modbus_RTU_send(slave_msg_buf->req.data,&slave_msg_buf->req.len, device_ID);
+    modify_CRC_in_msg_frame();
     mock_USART_RX_IRQ(); // To simulate that some char was recived after using modbus_RTU_send func to generate recived request
 
     TEST_ASSERT_EQUAL(MODBUS_SLAVE_IDLE, slave_manager_state_machine);
@@ -132,3 +161,12 @@ TEST(Slave_RTU_test, GivenModbusSlaveInitAndReadCoilsReqWithProperSlaveIdAndReci
 // {
 //     TEST_FAIL_MESSAGE("ADDED_NEW_TEST")
 // }
+static void modify_CRC_in_msg_frame(void)
+{
+    modbus_CRC_t CRC;
+
+    CRC= read_u16_from_buf(&slave_msg_buf->req.data[slave_msg_buf->req.len -2]);
+    CRC++;
+    write_u16_to_buf(&slave_msg_buf->req.data[slave_msg_buf->req.len -2],CRC);
+
+}
