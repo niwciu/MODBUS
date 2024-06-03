06cdd7654326ae13621e193e32dfe7669b913159
niwciu <57457942+niwciu@users.noreply.github.com>
2024-06-03 13:53:17 +0200

    REFACTOR: slave init procedure refactor

--- src/modbus_slave.c
+++ src/modbus_slave.c
@@ -37,7 +37,10 @@ PRIVATE modbus_status_flag_t TIMER_1_5_CHAR_FLAG = MODBUS_FLAG_UNKNOWN;
 PRIVATE modbus_status_flag_t TIMER_3_5_CHAR_FLAG = MODBUS_FLAG_UNKNOWN;
 PRIVATE modbus_status_flag_t FRAME_ERROR_FLAG = MODBUS_FLAG_UNKNOWN;
 
+static void init_modbus_data_buffers_and_queues(modbus_mode_t mode);
 static void register_msg_req_resp_data_buffers(modbus_mode_t mode);
+static void init_modbus_driver (modbus_mode_t mode, baud_t baud_rate, parity_t parity);
+static void init_modbus_slave_internall_data(modbus_device_ID_t Slave_ID);
 static void push_all_available_msg_buffer_to_free_queue(void);
 static void modbus_resp_send_callback(void);
 static void modbus_T_1_5_char_expired_callback(void);
@@ -46,24 +49,11 @@ static void modbus_frame_error_callback(void);
 
 void modbus_slave_init(modbus_mode_t mode, baud_t baud_rate, parity_t parity, modbus_device_ID_t slave_ID)
 {
-    register_msg_req_resp_data_buffers(mode);
-    modbus_queue_init(slave_tx_rx_q);
-    modbus_queue_init(slave_free_q);
-    push_all_available_msg_buffer_to_free_queue();
 
-    slave_RTU_driver = get_slave_RTU_driver_interface();
-    slave_RTU_driver->init(baud_rate, parity);
-    slave_msg_buf = modbus_queue_pop(slave_free_q);
-    slave_RTU_driver->enable_rcev(&slave_msg_buf->resp);
-    slave_RTU_driver->subscribe_msg_tx_done_cb(modbus_resp_send_callback);
-    slave_RTU_driver->subscribe_t_1_5_char_expired_cb(modbus_T_1_5_char_expired_callback);
-    slave_RTU_driver->subscribe_t_3_5_char_expired_cb(modbus_T_3_5_char_expired_callback);
-    slave_RTU_driver->subscribe_modbus_frame_error_cb(modbus_frame_error_callback);
-    modbus_slave_ID = slave_ID;
-    slave_manager_state_machine = MODBUS_SLAVE_IDLE;
-    TIMER_1_5_CHAR_FLAG = MODBUS_FLAG_CLEARED;
-    TIMER_3_5_CHAR_FLAG = MODBUS_FLAG_CLEARED;
-    FRAME_ERROR_FLAG = MODBUS_FLAG_CLEARED;
+
+    init_modbus_data_buffers_and_queues(mode);
+    init_modbus_driver(mode,baud_rate,parity);
+    init_modbus_slave_internall_data(slave_ID);
 }
 
 void check_modbus_request(void)
@@ -110,6 +100,14 @@ void check_modbus_request(void)
         break;
     }
 }
+static void init_modbus_data_buffers_and_queues(modbus_mode_t mode)
+{
+    register_msg_req_resp_data_buffers(mode);
+    modbus_queue_init(slave_tx_rx_q);
+    modbus_queue_init(slave_free_q);
+    push_all_available_msg_buffer_to_free_queue();
+    slave_msg_buf = modbus_queue_pop(slave_free_q);
+}
 static void register_msg_req_resp_data_buffers(modbus_mode_t mode)
 {
     if (RTU == mode)
@@ -120,6 +118,35 @@ static void register_msg_req_resp_data_buffers(modbus_mode_t mode)
             slave_msg[i].resp.data = &RTU_resp_buf[i][0];
         }
     }
+}
+
+static void init_modbus_driver (modbus_mode_t mode, baud_t baud_rate, parity_t parity)
+{
+    if(RTU == mode)
+    {
+        slave_RTU_driver = get_slave_RTU_driver_interface();
+        slave_RTU_driver->init(baud_rate, parity);
+        slave_RTU_driver->enable_rcev(&slave_msg_buf->resp);
+        slave_RTU_driver->subscribe_msg_tx_done_cb(modbus_resp_send_callback);
+        slave_RTU_driver->subscribe_t_1_5_char_expired_cb(modbus_T_1_5_char_expired_callback);
+        slave_RTU_driver->subscribe_t_3_5_char_expired_cb(modbus_T_3_5_char_expired_callback);
+        slave_RTU_driver->subscribe_modbus_frame_error_cb(modbus_frame_error_callback);
+    }
+    else
+    {
+        slave_RTU_driver=NULL;
+    }
+}
+
+static void init_modbus_slave_internall_data(modbus_device_ID_t Slave_ID)
+{
+    modbus_slave_ID = Slave_ID;
+
+    slave_manager_state_machine = MODBUS_SLAVE_IDLE;
+
+    TIMER_1_5_CHAR_FLAG = MODBUS_FLAG_CLEARED;
+    TIMER_3_5_CHAR_FLAG = MODBUS_FLAG_CLEARED;
+    FRAME_ERROR_FLAG = MODBUS_FLAG_CLEARED;
 }
 
 static void push_all_available_msg_buffer_to_free_queue(void)
