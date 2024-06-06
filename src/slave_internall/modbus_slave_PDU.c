9efcc5168b8dad85383414abbe1d0cf558a55674
niwciu <57457942+niwciu@users.noreply.github.com>
2024-06-06 16:22:52 +0200

    GREEN:  test ->
    WhenSlaveReciveReadCoilsRequestWithIncorrectStartingAddressAndQuantitiofOutputsThenSlaveRespondWithExceptionCode02

--- src/slave_internall/modbus_slave_PDU.c
+++ src/slave_internall/modbus_slave_PDU.c
@@ -85,7 +85,7 @@ static modbus_ret_t modbus_slave_read_coils(modbus_msg_t *modbus_msg)
             modbus_msg->resp.data[MODBUS_RESP_ERROR_CODE_IDX]= MODBUS_ERROR_CODE_MASK | MODBUS_REQUEST_DATA_QUANTITY_ERROR;
             status = RET_ERROR;
         }
-        else if(adr > MAIN_APP_COILS_QTY)
+        else if((adr > MAIN_APP_COILS_QTY)||((adr+coil_qty)>MAIN_APP_COILS_QTY))
         {
             modbus_msg->resp.data[MODBUS_RESP_ERROR_CODE_IDX]= MODBUS_ERROR_CODE_MASK | MODBUS_REQUEST_ADRES_RANGE_ERROR;
             status = RET_ERROR;
