48a9399c0635727ef0c1d21b994bedc597c07c89
niwciu <57457942+niwciu@users.noreply.github.com>
2024-06-08 12:57:50 +0200

    RED: added new test ->
    WhenSlaveReciveReadSicreteInputsRequestWithDiscreteInputsQtyToReadAboveAllowedValueThenSlaveRespondWithExceptionCode03

--- test/modbus/slave_PDU_exception_code_test_runner.c
+++ test/modbus/slave_PDU_exception_code_test_runner.c
@@ -9,8 +9,15 @@ TEST_GROUP_RUNNER(Slave_PDU_exception_code)
    RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadCoilsRequestWithCoilsQtyToReadEqual0ThenSlaveRespondWithExceptionCode03);
    RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadCoilsRequestWithIncorrectStartingAddresThenSlaveRespondWithExceptionCode02);
    RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadCoilsRequestWithIncorrectStartingAddressAndQuantitiofOutputsThenSlaveRespondWithExceptionCode02);
-   // Don't see place for adopting exception code 04
+   // Don't see place for adopting exception code 04 while if coil is not registered it will retunr 0
    // RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadCoilsRequestAndCatchErrorDuringProcessingOfRequestThenSlaveRespondWithExceptionCode04);
+
+   RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadSicreteInputsRequestWithDiscreteInputsQtyToReadAboveAllowedValueThenSlaveRespondWithExceptionCode03);
+   // RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadSicreteInputsRequestWithDiscreteInputsQtyToReadEqual0ThenSlaveRespondWithExceptionCode03);
+   // RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadSicreteInputsRequestWithIncorrectStartingAddresThenSlaveRespondWithExceptionCode02);
+   // RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadSicreteInputsRequestWithIncorrectStartingAddressAndQuantitiofOutputsThenSlaveRespondWithExceptionCode02);
+   // Don't see place for adopting exception code 04 while if coil is not registered it will retunr 0
+   // RUN_TEST_CASE(Slave_PDU_exception_code, WhenSlaveReciveReadSicreteInputsRequestAndCatchErrorDuringProcessingOfRequestThenSlaveRespondWithExceptionCode04);
    
    // RUN_TEST_CASE(Slave_PDU_exception_code, );
    // RUN_TEST_CASE(Slave_PDU_exception_code, );
