2474eabd67f8c152542d4ec15d6a4d9266fad0a5
niwciu <57457942+niwciu@users.noreply.github.com>
2024-06-08 14:21:14 +0200

    GREEN: added new test -> WhenSlaveReciveReadHoldingRegisterRequestWithHoldingRegisterQtyToReadEqual0ThenSlaveRespondWithExceptionCode03

--- test/modbus/slave_PDU_exception_code_test.c
+++ test/modbus/slave_PDU_exception_code_test.c
@@ -217,10 +217,18 @@ TEST(Slave_PDU_exception_code, WhenSlaveReciveReadHoldingRegisterRequestWithHold
     TEST_ASSERT_EQUAL_INT16(MODBUS_ERROR_CODE_MASK | MODBUS_REQUEST_DATA_QUANTITY_ERROR, RTU_msg->resp.data[MODBUS_RESP_ERROR_CODE_IDX]);
 }
 
-// TEST(Slave_PDU_exception_code, )
-// {
-//     TEST_FAIL_MESSAGE("ADDED NEW TEST !!!");
-// }
+TEST(Slave_PDU_exception_code, WhenSlaveReciveReadHoldingRegisterRequestWithHoldingRegisterQtyToReadEqual0ThenSlaveRespondWithExceptionCode03)
+{
+    modbus_adr_t in_reg_adr = 0x0000;
+
+    modbus_master_read_holding_reg_req(RTU_msg, in_reg_adr, MODBUS_MAX_READ_REG_QTY);
+    set_zero_obj_qty_in_req_frame(RTU_msg);
+
+    parse_master_request_and_prepare_resp(RTU_msg);
+    
+    TEST_ASSERT_EQUAL(MODBUS_READ_HOLDING_REGISTERS_FUNC_CODE,RTU_msg->resp.data[MODBUS_FUNCTION_CODE_IDX]);
+    TEST_ASSERT_EQUAL_INT16(MODBUS_ERROR_CODE_MASK | MODBUS_REQUEST_DATA_QUANTITY_ERROR, RTU_msg->resp.data[MODBUS_RESP_ERROR_CODE_IDX]);
+}
 
 // TEST(Slave_PDU_exception_code, )
 // {
