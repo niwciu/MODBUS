32018e55f19f0e23f1a7442607e212e4c5850506
niwciu <57457942+niwciu@users.noreply.github.com>
2024-06-25 21:00:57 +0200

    GREEN: Added new test -> GivenModbusQueueInitWhenModbusMasgPushToModbusQueueThenHeadisEqualTo1AndTailIsEqualTo0

--- test/modbus/modbus_queue_test.c
+++ test/modbus/modbus_queue_test.c
@@ -6,11 +6,19 @@
 TEST_GROUP(Modbus_Queue_test);
 modbus_queue_t test_queue;
 modbus_queue_t *q;
+modbus_buf_t req_buf[5];
+modbus_buf_t resp_buf[5];
+modbus_msg_t msg_buf[5];
 
 TEST_SETUP(Modbus_Queue_test)
 {
     /* Init before every test */
     q=&test_queue;
+    for(uint8_t i=0; i<5; i++)
+    {
+        msg_buf[i].req.data=&req_buf[i];
+        msg_buf[i].resp.data=&resp_buf[i];
+    }
 }
 
 TEST_TEAR_DOWN(Modbus_Queue_test)
@@ -25,10 +33,13 @@ TEST(Modbus_Queue_test, WhenModbusQueueInitThenQueueHeadAndTailIsEqualToZero)
     TEST_ASSERT_EQUAL(0,q->tail);
 }
 
-// TEST(Modbus_Queue_test, )
-// {
-//     TEST_FAIL_MESSAGE("Implement your test!");
-// }
+TEST(Modbus_Queue_test, GivenModbusQueueInitWhenModbusMasgPushToModbusQueueThenHeadisEqualTo1AndTailIsEqualTo0)
+{
+    modbus_queue_init(q);
+    modbus_queue_push(q,&msg_buf[0]);
+    TEST_ASSERT_EQUAL(1,q->head);
+    TEST_ASSERT_EQUAL(0,q->tail);
+}
 
 // TEST(Modbus_Queue_test, )
 // {
