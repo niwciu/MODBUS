9cfa06997ca86d2a44a8eea6d871a0dfd097b037
niwciu <57457942+niwciu@users.noreply.github.com>
2024-06-29 18:18:50 +0200

    MASTER_HW_test: first part that working - sending req on serial line

--- test/hw_test/STM32G070RB_MODBUS_MASTER/Core/Src/main.c
+++ test/hw_test/STM32G070RB_MODBUS_MASTER/Core/Src/main.c
@@ -25,18 +25,19 @@
 #include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
-  core_init();
-  // modbus_master_init
+    core_init();
+    modbus_master_init(RTU, 115200, ODD);
+    // __enable_irq();
 
-  __enable_irq();
+    modbus_master_read_coils(0x0001, 0x002, 0x03);
     /* Loop forever */
-	while(1)
-  {
-
-  }
+    while (1)
+    {
+        check_modbus_master_manager();
+    }
 }
