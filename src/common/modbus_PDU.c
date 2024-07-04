efe541cde53fc13a3a9b0dab004017d94e74be9a
niwciu <57457942+niwciu@users.noreply.github.com>
2024-07-05 00:14:21 +0200

    REFACTOR: common modbus PDU doxygen comments added

--- src/common/modbus_PDU.c
+++ src/common/modbus_PDU.c
@@ -1,15 +1,27 @@
 /**
  * @file modbus_PDU_common.c
+ * @brief Implementation file for common Modbus Protocol Data Unit (PDU) operations.
+ *
+ * This file implements functions declared in modbus_PDU_common.h for handling
+ * Modbus Protocol Data Unit (PDU) operations, specifically related to coils and discrete inputs.
+ *
  * @author niwciu (niwciu@gmail.com)
- * @brief
  * @version 0.0.1
  * @date 2024-06-28
  *
  * @copyright Copyright (c) 2024
- *
  */
 #include "modbus_PDU.h"
 
+/**
+ * @brief Calculates the byte count required for a given quantity of coils or discrete inputs.
+ *
+ * This function calculates the number of bytes required to represent the specified quantity
+ * of coils or discrete inputs. Each coil or input is represented by a single bit.
+ *
+ * @param coil_qty Number of coils or discrete inputs.
+ * @return modbus_byte_count_t - Byte count needed to store the coils or discrete inputs.
+ */
 modbus_byte_count_t get_coil_din_byte_count(modbus_data_qty_t coil_qty)
 {
     modbus_byte_count_t byte_count;
@@ -22,6 +34,15 @@ modbus_byte_count_t get_coil_din_byte_count(modbus_data_qty_t coil_qty)
     return byte_count;
 }
 
+/**
+ * @brief Clears the status byte buffer for coils or discrete inputs.
+ *
+ * This function clears (sets to 0) the status byte buffer used for storing the status
+ * of coils or discrete inputs.
+ *
+ * @param buf Pointer to the buffer holding the status bytes.
+ * @param qty Number of Coil or Discrete Inputs status bytes to clear.
+ */
 void clear_coil_din_status_byte(modbus_buf_t *buf, modbus_data_qty_t qty)
 {
     for (uint8_t i = 0; i < qty; i++)
--- src/common/modbus_PDU.h
+++ src/common/modbus_PDU.h
@@ -1,12 +1,15 @@
 /**
  * @file modbus_PDU_common.h
+ * @brief Header file for common Modbus Protocol Data Unit (PDU) operations.
+ *
+ * This header file declares functions related to Modbus Protocol Data Unit (PDU) operations,
+ * specifically for coil and discrete input data handling.
+ *
  * @author niwciu (niwciu@gmail.com)
- * @brief
  * @version 0.0.1
  * @date 2024-06-28
  *
  * @copyright Copyright (c) 2024
- *
  */
 #ifndef _MODBUS_PDU_COMMON_H_
 #define _MODBUS_PDU_COMMON_H_
@@ -19,7 +22,6 @@ extern "C"
 
     modbus_byte_count_t get_coil_din_byte_count(modbus_data_qty_t coil_qty);
     void clear_coil_din_status_byte(modbus_buf_t *buf, modbus_data_qty_t qty);
-    // modbus_ret_t set_coil_din_value_from_modbus_msg(const modbus_buf_t *data_state_ptr, modbus_adr_t start_adr, modbus_data_qty_t coil_din_qty, modbus_coil_disin_t **data_tab);
 
 #ifdef __cplusplus
 }
