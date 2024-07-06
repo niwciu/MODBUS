38c60b36c3491d2e631da35e2070a6b3c9f71a69
niwciu <57457942+niwciu@users.noreply.github.com>
2024-07-06 15:52:09 +0200

    GREEN: added new test -> GivenModbusMasterInRTUmodeInitAndAnyRequestTransmittedAndMsgRecivedWhenTimer_1_5CharExpiredAndNoRtuErrorInRespMsgThenModbusMasterRespRecivedStateSet

--- test/modbus/master_RTU_test.c
+++ test/modbus/master_RTU_test.c
@@ -301,10 +301,21 @@ TEST(Master_RTU_test, GivenModbusMasterInRTUmodeInitAndAnyRequestTransmittedAndM
     check_modbus_master_manager();
     TEST_ASSERT_EQUAL(0, modbus_master_resp_timeout_timer);
 }
-// TEST(Master_RTU_test, GivenModbusMasterInRTUmodeInitAndAnyRequestTransmittedAndMsgRecivedWhenTimer_1_5CharExpiredAndNoRtuErrorInRespMsgThenModbusMasterRespRecivedStateSet)
-// {
-//     TEST_FAIL_MESSAGE("Implement your test!");
-// }
+TEST(Master_RTU_test, GivenModbusMasterInRTUmodeInitAndAnyRequestTransmittedAndMsgRecivedWhenTimer_1_5CharExpiredAndNoRtuErrorInRespMsgThenModbusMasterRespRecivedStateSet)
+{
+    modbus_adr_t coil_adr = 0x0002;
+    modbus_device_ID_t slave_ID = 0x09;
+    modbus_data_qty_t coils_qty = 2;
+    modbus_coil_disin_t readed_coil_disin[coils_qty];
+
+    modbus_master_read_coils(coil_adr, coils_qty, slave_ID, readed_coil_disin);
+    generate_send_req_sequence();
+    generate_resp_using_slave_lib(slave_ID);
+
+    generate_msg_T_1_5_char_brake_sequence();
+    check_modbus_master_manager();
+    TEST_ASSERT_EQUAL(MODBUS_MASTER_RESP_RECIVED, modbus_master_manager_state_machine);
+}
 
 //  MODBUS_MASTER_RESP_WAITING state tests
 //  resp msg recived correct
