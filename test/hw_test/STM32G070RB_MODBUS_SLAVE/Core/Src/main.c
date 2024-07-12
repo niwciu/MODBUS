ae7a12306446be13dfff15fedbbefdbef3657a86
niwciu <57457942+niwciu@users.noreply.github.com>
2024-07-12 21:06:50 +0200

    HW_test: added in reg in test app

--- test/hw_test/STM32G070RB_MODBUS_SLAVE/Core/Src/main.c
+++ test/hw_test/STM32G070RB_MODBUS_SLAVE/Core/Src/main.c
@@ -28,6 +28,7 @@
 
 modbus_coil_disin_t coils[20] = {1,0,1,0,0,1,0,1,1,1,0,0,0,1,1,1,1,0,0,1};
 modbus_reg_t hreg[20] = {2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,3001,3002,3003,3004,3005,3006,3007,3008,3009,3010};
+modbus_reg_t inreg[20] = {4201, 4202, 4203, 4204, 4205, 4206, 4207, 4208, 4209, 4210, 4301, 4302, 4303, 4304, 4305, 4306, 4307, 4308, 4309, 4310};
 
 int main(void)
 {
@@ -37,6 +38,7 @@ int main(void)
   {
     register_app_data_to_modbus_slave_coils_table(0x0000+i,&coils[i]);
     register_app_data_to_modbus_slave_hreg_table(0x0000+i,&hreg[i]);
+    register_app_data_to_modbus_slave_inreg_table(0x0000 + i, &inreg[i]);
   }
   
   /* Loop forever */
