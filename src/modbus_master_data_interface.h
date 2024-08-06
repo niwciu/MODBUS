1e56ee4cbbe196fd863f85b4adfa0c3d279e0a89
niwciu <57457942+niwciu@users.noreply.github.com>
2024-08-06 09:27:57 +0200

    Modbus: added week atrubute to modbus data interface

--- src/modbus_master_data_interface.h
+++ src/modbus_master_data_interface.h
@@ -23,20 +23,20 @@
 
 #include "modbus_public_type.h"
 
-void modbus_master_coil_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_coil_disin_t coil_val) __attribute__((weak));
+void __attribute__((__weak__))modbus_master_coil_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_coil_disin_t coil_val);
 
-void modbus_master_coil_write(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_data_qty_t coil_qty) __attribute__((weak));
-void modbus_master_coil_exception(const modbus_read_data_t *resp_data) __attribute__((weak));
+void __attribute__((__weak__)) modbus_master_coil_write(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_data_qty_t coil_qty);
+void __attribute__((__weak__)) modbus_master_coil_exception(const modbus_read_data_t *resp_data);
 
-void modbus_master_disin_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_coil_disin_t disin_val) __attribute__((weak));
-void modbus_master_disin_exception(const modbus_read_data_t *resp_data) __attribute__((weak));
+void __attribute__((__weak__)) modbus_master_disin_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_coil_disin_t disin_val);
+void __attribute__((__weak__)) modbus_master_disin_exception(const modbus_read_data_t *resp_data);
 
-void modbus_master_hreg_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_reg_t hreg_val) __attribute__((weak));
-void modbus_master_hreg_write(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_data_qty_t reg_qty) __attribute__((weak));
-void modbus_master_hreg_exception(const modbus_read_data_t *resp_data) __attribute__((weak));
+void __attribute__((__weak__)) modbus_master_hreg_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_reg_t hreg_val);
+void __attribute__((__weak__)) modbus_master_hreg_write(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_data_qty_t reg_qty);
+void __attribute__((__weak__)) modbus_master_hreg_exception(const modbus_read_data_t *resp_data);
 
-void modbus_master_inreg_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_reg_t inreg_val) __attribute__((weak));
-void modbus_master_inreg_exception(const modbus_read_data_t *resp_data) __attribute__((weak));
+void __attribute__((__weak__)) modbus_master_inreg_read(modbus_device_ID_t slave_adr, modbus_adr_t data_adr, modbus_reg_t inreg_val);
+void __attribute__((__weak__)) modbus_master_inreg_exception(const modbus_read_data_t *resp_data);
 
-void modbus_master_data_timeout_error(const modbus_master_error_report_t *timeout_error_rep) __attribute__((weak));
-void modbus_master_communication_error(const modbus_master_error_report_t *communication_error_rep) __attribute__((weak));
+void __attribute__((__weak__)) modbus_master_data_timeout_error(const modbus_master_error_report_t *timeout_error_rep);
+void __attribute__((__weak__)) modbus_master_communication_error(const modbus_master_error_report_t *communication_error_rep);
