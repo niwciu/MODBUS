93a8bd611d539f76ab680d23c94fe329f95a1a5e
niwciu <niwciu@gmail.com>
2024-11-21 14:54:18 +0100

    removed lizart functionality test function

--- src/modbus_slave.c
+++ src/modbus_slave.c
@@ -54,7 +54,6 @@ static void modbus_resp_send_callback(void);
 static void modbus_T_1_5_char_expired_callback(void);
 static void modbus_T_3_5_char_expired_callback(void);
 static void modbus_frame_error_callback(void);
-static void lizart_test_function(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e);
 
 /**
  * @brief Registers application data to Modbus slave coils table.
@@ -361,79 +360,4 @@ static void modbus_T_3_5_char_expired_callback(void)
 static void modbus_frame_error_callback(void)
 {
     FRAME_ERROR_FLAG = MODBUS_FLAG_SET;
-}
-
-static void lizart_test_function(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e)
-{
-    switch (a)
-    {
-    case 1:
-        a = b;
-        b = c;
-        c = d;
-        d = a;
-        a++;
-        break;
-    case 2:
-        a++;
-        break;
-    case 3:
-        b++;
-        break;
-    case 4:
-        c++;
-        break;
-    case 5:
-        a++;
-        c++;
-        break;
-    case 6:
-        d++;
-        a--;
-        break;
-    case 7:
-        c++;
-        d--;
-        a++;
-        break;
-    case 8:
-        c--;
-        d--;
-        a++;
-        break;
-    case 9:
-        d = a;
-        a++;
-        a = b;
-        b = c;
-        break;
-    case 10:
-        a++;
-        a = b;
-        b = c;
-        c = d;
-        d = a;
-        a++;
-        break;
-    case 11:
-        b = c;
-        c = d;
-        break;
-    case 12:
-        a++;
-        a++;
-        a++;
-        break;
-    case 13:
-        d--;
-        d--;
-
-        break;
-    default:
-        c--;
-        a--;
-        b++;
-        d++;
-        break;
-    }
 }
